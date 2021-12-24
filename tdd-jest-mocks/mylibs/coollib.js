409809710462cab5dbb81e314f5e6bab0a50bcd3
Luiz Fernando Calaça Silva <luizcalaca@Luizs-MacBook-Air.local>
2021-12-24 04:46:32 -0300

    mock test

---
+++ tdd-jest-mocks/mylibs/coollib.js
@@ -0,0 +1,9 @@
+
+const fs = require('fs');
+ 
+function findTestTxt() {
+    const files = fs.readdirSync('./');
+    return files.some(f => f === 'test.txt');
+}
+ 
+module.exports = { findTestTxt }
